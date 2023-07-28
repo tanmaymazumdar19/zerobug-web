1cec3f8ddc61bcc91bfe9a604992c1c548372f79
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-07-29 02:59:19 +0530

    fix: unused vars

--- src/pages/Company/GetHired/EmployeeDetails.tsx
+++ src/pages/Company/GetHired/EmployeeDetails.tsx
@@ -1,29 +1,25 @@
-import React from "react";
 import InputTypeTextAndEmail from "../../../components/Reuseable/Inputcomp";
 import { Stack } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { StyledForm } from "../../Login/style";
 
 const defaultValues: any = [
-  {name:"empName", value: "", type: "text", label: "Employee Name" },
-  {name:"empAge", value: "", type: "text", label: "Employee Age" },
- {name:"empGender", value: "", type: "text", label: "Employee Gender" },
-  {name:"compName", value: "", type: "text", label: "Company Name" },
-  {name:"role", value: "", type: "text", label: "Role" },
-   {name:"techStack", value: "", type: "text", label: "Tech Stack" },
-   {name:"experience", value: "", type: "text", label: "Experience" },
-   {name:"availability", value: "", type: "text", label: "Availability" },
-   {name:"resume", value: "", type: "text", label: "Resume" },
+  { name: "empName", value: "", type: "text", label: "Employee Name" },
+  { name: "empAge", value: "", type: "text", label: "Employee Age" },
+  { name: "empGender", value: "", type: "text", label: "Employee Gender" },
+  { name: "compName", value: "", type: "text", label: "Company Name" },
+  { name: "role", value: "", type: "text", label: "Role" },
+  { name: "techStack", value: "", type: "text", label: "Tech Stack" },
+  { name: "experience", value: "", type: "text", label: "Experience" },
+  { name: "availability", value: "", type: "text", label: "Availability" },
+  { name: "resume", value: "", type: "text", label: "Resume" },
 ];
-
 
 const EmployeeDetails = () => {
   const {
-    watch,
     control,
     register,
     handleSubmit,
-    formState: { errors },
     reset,
   } = useForm();
   return (
@@ -35,7 +31,7 @@ const EmployeeDetails = () => {
           reset();
         })}
       >
-        {(defaultValues).map((item: any) => {
+        {defaultValues.map((item: any) => {
           console.log("item", item);
           return (
             <InputTypeTextAndEmail
