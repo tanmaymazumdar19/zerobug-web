4ff3cb94137982e4e6672352c06cd34805a88a2b
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-07-28 20:51:35 +0530

    feat: folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { Suspense } from 'react'
 import Routes from './route'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+console.log('Hello');
+
 const App = () => {
   return (
     <Router>
