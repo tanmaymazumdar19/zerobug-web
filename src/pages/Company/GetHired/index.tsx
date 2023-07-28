4fd123d1a380ea82c390aa90d0dcc8987c30613f
Shubham Chaudhary <shubham.chaudhary@quokkalabs.com>
2023-07-28 22:42:07 +0530

    (feat):resolve npm run  build issue

--- src/pages/Company/GetHired/index.tsx
+++ src/pages/Company/GetHired/index.tsx
@@ -84,7 +84,7 @@ export default function GetHired() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePage = (_event: unknown, newPage: number) => {
     setPage(newPage);
   };
 
