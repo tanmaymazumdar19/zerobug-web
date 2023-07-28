f544898c20981750ac587179f5bdcc717170c31d
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-07-28 23:29:15 +0530

    feat: get hired company company listing

--- src/pages/Company/GetHired/index.tsx
+++ src/pages/Company/GetHired/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  Box,
   Paper,
   Table,
   TableBody,
@@ -8,6 +9,7 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  Typography,
 } from "@mui/material";
 
 interface Column {
@@ -62,6 +64,19 @@ function createData(
   return { name, code, population, size, density };
 }
 
+const companyDetails = [
+  {
+    companyName: "Quokkalabs",
+    companyEmail: "quokkalabs.com",
+    companyDesc:
+      "We as company provide digital solution to world by mobile app and web app",
+    companyEstd: "2019",
+    companyStrength: "70",
+    techStack:
+      "Mobile Development, Web App Development, DevOps solution, Backend Solution",
+  },
+];
+
 const rows = [
   createData("India", "IN", 1324171354, 3287263),
   createData("China", "CN", 1403500365, 9596961),
@@ -96,53 +111,84 @@ export default function GetHired() {
   };
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <>
+      <Box mb={3}>
+        <CustomTypography>
+          Company Name : <b>{companyDetails[0].companyName}</b>
+        </CustomTypography>
+        <CustomTypography>
+          Company Email : <b>{companyDetails[0].companyEmail}</b>
+        </CustomTypography>
+        <CustomTypography>
+          Company Description : <b>{companyDetails[0].companyDesc}</b>
+        </CustomTypography>
+        <CustomTypography>
+          Company Established : <b>{companyDetails[0].companyEstd}</b>
+        </CustomTypography>
+        <CustomTypography>
+          Company Strength : <b>{companyDetails[0].companyStrength}</b>
+        </CustomTypography>
+        <CustomTypography>
+          Tech Stack : <b>{companyDetails[0].techStack}</b>
+        </CustomTypography>
+      </Box>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
   );
 }
+
+const CustomTypography = ({ children }: { children: React.ReactNode }) => {
+  return <Typography mb={1}>{children}</Typography>;
+};
