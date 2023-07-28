bf012dff073a08fe16e1876906ec2ad4dd8b5a79
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-07-28 22:35:19 +0530

    feat: employee list of companyies

--- src/pages/Company/DoHire/Employee.tsx
+++ src/pages/Company/DoHire/Employee.tsx
@@ -1,81 +1,61 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: "name" | "email" | "tech stack" | "availability" | "action";
   label: string;
   minWidth?: number;
-  align?: 'right';
+  align?: "right" | "left";
   format?: (value: number) => string;
 }
 
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "email", label: "email", minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: "availability",
+    label: "Availability",
     minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
+    align: "left",
+    format: (value: number) => value.toLocaleString("en-US"),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: "action",
+    label: "Action",
     minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
+    align: "right",
+    format: (value: number) => value.toLocaleString("en-US"),
   },
 ];
 
 interface Data {
   name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  email: string;
+  availability: "yes" | "no";
 }
 
 function createData(
   name: string,
-  code: string,
-  population: number,
-  size: number,
+  email: string,
+  availability: "yes" | "no"
 ): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
+  return { name, email, availability };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData("Shakeeb", "shakeeb.arsalan@quokkalabs.com", "yes"),
+  createData("Tanmay", "tanmay.mazumdar@quokkalabs.com", "yes"),
+  createData("Shubham", "shubham.chaudhary@quokkalabs.com", "yes"),
+  createData("Vinayak", "vinayak.@quokkalabs.com", "yes"),
+  createData("Anshul", "anhsul.dimri@quokkalabs.com", "yes"),
+  createData("Risalat", "rishalat.shamoa@quokkalabs.com", "yes"),
 ];
 
 export default function StickyHeadTable() {
@@ -86,14 +66,16 @@ export default function StickyHeadTable() {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
@@ -111,14 +93,14 @@ export default function StickyHeadTable() {
           <TableBody>
             {rows
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((row: any) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.email}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
+                          {column.format && typeof value === "number"
                             ? column.format(value)
                             : value}
                         </TableCell>
