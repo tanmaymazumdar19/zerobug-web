de0a5437040012e19e148fb6c36fb8754838c1e9
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2023-07-29 06:54:07 +0530

    feat: Major UI design on admin panel.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,50 +1,94 @@
-import { useLayoutEffect, useState } from "react";
-import { useTheme } from "@mui/material/styles";
+import { useLayoutEffect } from "react";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
 import MainContent from "./Content";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Avatar, Typography } from "@mui/material";
+
+const SidebarNavLinks = [
+  // { path: "/company/dashboard", title: "Dashboard" },
+  { path: "/company/employee/hire", title: "Do Hire" },
+  { path: "/company/employee/get-hired", title: "Get Hired" },
+];
+
+const AdminLinks = [
+  { path: "/admin", title: "Companies" },
+];
 
 export default function MiniDrawer() {
   const navigate = useNavigate();
   const authToken = useSelector((state: any) => state?.authSlice?.userToken);
-  const { pathname } = useLocation();
-  const isAdmin = useSelector((state: any) => state?.authSlice?.isAdmin)
+  const { pathname, state } = useLocation();
+  const isAdmin = useSelector((state: any) => state?.authSlice?.isAdmin);
+
+  const handleNavigate = (path: string, title: string) => {
+    navigate(path, { state: {title} });
+  };
 
   useLayoutEffect(() => {
     if (!authToken) {
       navigate("/login");
     }
 
-    if (pathname === '/' && isAdmin) {
-      navigate('/admin')
+    if (pathname === "/" && isAdmin) {
+      navigate("/admin");
     }
   }, []);
 
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
   return (
     <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Header open={open} handleHeaderOpen={handleDrawerOpen} />
-      <Sidebar
-        open={open}
-        theme={theme}
-        handleDrawerClose={handleDrawerClose}
-      />
-      <MainContent />
+      <Box sx={{ display: "flex", width: "100%", height: "100vh" }}>
+        <Box sx={{ minWidth: 320, bgcolor: "#f1f1f4", p: "52px 0" }}>
+        {(isAdmin ? AdminLinks : SidebarNavLinks).map(({ path, title }) => (
+          <Box
+            key={path}
+            sx={{
+              fontFamily: "'Inter', sans-serif",
+              p: "12px 36px",
+              cursor: "pointer",
+              bgcolor: path === pathname ? "#e1e1e5" : "transparent",
+              ":hover": { bgcolor: "#e6e6ea" },
+            }}
+            onClick={() => handleNavigate(path, title)}
+          >
+            {title}
+          </Box>
+        ))}
+        </Box>
+        <Box sx={{ width: "100%", p: "32px 48px", overflow: 'auto' }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+              {/* <IconButton
+                color="inherit"
+                aria-label="open Header"
+                // onClick={handleHeaderOpen}
+                edge="start"
+                sx={{
+                  // marginRight: 5,
+                  ...(open && { display: "none" }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton> */}
+
+              <Typography sx={{ fontSize: "24px", fontWeight: 300 }}>
+                {state?.title}
+              </Typography>
+            </Box>
+
+            <Avatar sx={{ width: 32, height: 32, cursor: 'pointer', fontSize: '12px' }}>TM</Avatar>
+          </Box>
+
+          <MainContent />
+        </Box>
+      </Box>
     </Box>
   );
 }
--- src/redux/api/middleware.ts
+++ src/redux/api/middleware.ts
@@ -1,7 +1,7 @@
 import { adminApis, companyApi } from './api'
 
 const middleware = {
-  middleware: (getDefaultMiddleware: any): any => getDefaultMiddleware().concat(adminApis.middleware).concat(companyApi.middleware),
+  middleware: (getDefaultMiddleware: any): any => getDefaultMiddleware().concat(adminApis.middleware,companyApi.middleware),
 }
 
 export default middleware
