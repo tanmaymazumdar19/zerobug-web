d06e852e408395a0bffb4c529e41825b882526bc
vinayak-0007 <vinayakkashyap149@gmail.com>
2023-07-29 06:10:29 +0530

    resolve error

--- src/redux/rootReducer/rootReducer.ts
+++ src/redux/rootReducer/rootReducer.ts
@@ -3,6 +3,8 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { adminApis, companyApi } from "../api/api";
 import authSlice from "../slices/authSlice";
 import modalSlice from "../slices/modalSlice";
+import getHiredSlice from "../slices/getHired";
+import notificationSlice from "../slices/notificationSlice";
 
 const rootReducer = combineReducers({
   authSlice,
