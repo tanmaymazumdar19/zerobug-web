6c4828787ce18558eb99f7b1d9b19d8a36547ccd
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2023-07-29 09:58:50 +0530

    feat: Add logout option on avatar click.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,13 @@
-import { useLayoutEffect } from 'react'
-import Box from '@mui/material/Box'
-import MainContent from './Content'
+import { MouseEvent, useLayoutEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Avatar, Typography } from '@mui/material'
+import { Avatar, Box, ListItemIcon, Menu, MenuItem, Typography } from '@mui/material'
+import Logout from '@mui/icons-material/Logout'
+
+import MainContent from './Content'
 import { resetAuthToken } from '../../redux/slices/authSlice'
 
 const SidebarNavLinks = [
-  // { path: "/company/dashboard", title: "Dashboard" },
   { path: '/company/employee/hire', title: 'Do Hire' },
   { path: '/company/employee/get-hired', title: 'Get Hired' },
 ]
@@ -20,9 +20,19 @@ export default function MiniDrawer() {
   const authToken = useSelector((state: any) => state?.authSlice?.userToken)
   const { pathname, state } = useLocation()
   const isAdmin = useSelector((state: any) => state?.authSlice?.isAdmin)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
   const handleNavigate = (path: string, title: string) => {
     navigate(path, { state: { title } })
+  }
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   }
 
   const tryLogout = () => {
@@ -95,10 +105,56 @@ export default function MiniDrawer() {
                 cursor: 'pointer',
                 fontSize: '12px',
               }}
-              onClick={() => tryLogout()}
+              onClick={handleClick}
             >
               TM
             </Avatar>
+
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem onClick={() => {
+                handleClose()
+                tryLogout()
+              }}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
           </Box>
 
           <MainContent />
